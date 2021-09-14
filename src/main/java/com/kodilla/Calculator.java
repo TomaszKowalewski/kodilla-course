76c1cc7626b387b24c1a466565c7e6397ce7579b
Tomasz Kowalewski <tomaszkowalewski@MacBook-Pro-Tomasz.local>
2021-09-14 16:32:13 +0200

    calulator

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,12 +1,12 @@
 package com.kodilla;
 
 public class Calculator {
-
     public void someMethod() {
-    System.out.println("it is works");
+        System.out.println("It works!");
     }
-    public static void main (String []args) {
+    public static void main(String args[]) {
         Calculator calculator = new Calculator();
         calculator.someMethod();
+
     }
 }
