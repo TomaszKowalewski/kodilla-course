ab12d35a0c8a80a067be220512a2f9cf2efcc759
Tomasz Kowalewski <tomaszkowalewski@MacBook-Pro-Tomasz.local>
2021-11-05 11:00:29 +0100

    7.2 Stream

--- kodilla-testing/kodilla-stream/src/main/java/StreamMain.java
+++ kodilla-testing/kodilla-stream/src/main/java/StreamMain.java
@@ -1,4 +1,5 @@
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -28,7 +29,7 @@ public class StreamMain {
     poemBeautifier.beautify("If not now, then when?", "The spoon does not exist",(firstText, secondText) -> firstText.toUpperCase()+" "+secondText.toUpperCase());
     poemBeautifier.beautify("If not now, then when?", "The spoon does not exist",(firstText, secondText) -> "***"+" "+secondText+" "+"***"+" "+"***"+" "+firstText+" "+"***");
 
-
-
+    System.out.println("Using stream to generate even numbers from 1 to 20");
+    NumbersGenerator.generateEven(20);
     }
 }
